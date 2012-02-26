d3f1dcc87923889102ea38e5d01b86587480a06b
Gregory Burlet <gregory.burlet@mail.mcgill.ca>
2012-02-26 18:19:55 -0500

    hold out test data
    leave one song out to use as test data for model validation

--- learnHMM.py
+++ learnHMM.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 from emission import *
 
-def learnHMM(M, addOne = True, features = 't', covType = 'full'):
+def learnHMM(M, addOne = True, features = 't', covType = 'full', leaveOneOut = 3):
     '''
     PARAMETERS
     ----------
@@ -11,6 +11,8 @@ def learnHMM(M, addOne = True, features = 't', covType = 'full'):
     features: treble chroma 't' or bass chroma 'b' or both
     covType: type of the covariance matrix for the GMM emission distribution
              'diag', 'full'
+    leaveOneOut: songID to leave out of the training phase and save for model validation
+    
 
     RETURNS
     -------
@@ -30,6 +32,11 @@ def learnHMM(M, addOne = True, features = 't', covType = 'full'):
     numSongs = 0;
     pChordName = ""
     obsNum = 0
+    
+    # initialize hold out data storage
+    Xtest = []
+    ytest = []
+
     for obs in groundTruth:
         # skip header
         if obsNum == 0:
@@ -37,14 +44,14 @@ def learnHMM(M, addOne = True, features = 't', covType = 'full'):
             continue
         
         obs = obs.split(",")
-        sid = obs[0]
+        sid = int(obs[0])
 
-        if 't' in features:
-            trebleChroma = np.asarray(obs[2:14])[np.newaxis,:]
-        if 'b' in features:
-            bassChroma = np.asarray(obs[14:26])[np.newaxis,:]
-        if features == 'tb':
-            np.hstack((trebleChroma, bassChroma))
+        if features == 't':
+            chroma = np.asarray(obs[2:14], dtype=np.float)[np.newaxis,:]
+        elif features == 'b':
+            chroma = np.asarray(obs[14:26], dtype=np.float)[np.newaxis,:]
+        elif features == 'tb':
+            chroma = np.asarray(obs[2:26], dtype=np.float)[np.newaxis,:]
             
         simpleQuality = obs[35].strip()
         if simpleQuality != "NA":
@@ -58,36 +65,44 @@ def learnHMM(M, addOne = True, features = 't', covType = 'full'):
         if chordName.strip() == "NANA":
             continue
 
-        if sid != pSid:
-            # new training sequence
-            # update pi
-            if chordName in piDict:
-                piDict[chordName] += 1
-            else:
-                piDict[chordName] = 1
-            pSid = sid
-            pChordName = chordName
-            numSongs += 1
-        else:
-            #update A
-            if pChordName in aDict:
-                if chordName in aDict[pChordName]:
-                    aDict[pChordName][chordName] += 1.0
+        if sid != leaveOneOut:
+            if sid != pSid:
+                # new training sequence
+                # update pi
+                if chordName in piDict:
+                    piDict[chordName] += 1
                 else:
-                    aDict[pChordName][chordName] = 1.0
+                    piDict[chordName] = 1
+                pSid = sid
+                pChordName = chordName
+                numSongs += 1
             else:
-                aDict[pChordName] = {chordName: 1.0}
-        
-        # update B
-        if chordName in bDict:
-            bDict[chordName] = np.vstack((bDict[chordName], trebleChroma))
+                #update A
+                if pChordName in aDict:
+                    if chordName in aDict[pChordName]:
+                        aDict[pChordName][chordName] += 1.0
+                    else:
+                        aDict[pChordName][chordName] = 1.0
+                else:
+                    aDict[pChordName] = {chordName: 1.0}
+            
+            # update B
+            if chordName in bDict:
+                bDict[chordName] = np.vstack((bDict[chordName], chroma))
+            else:
+                bDict[chordName] = chroma.copy()
+
+            # update state labels
+            QLabels.add(chordName)
+
+            obsNum += 1
         else:
-            bDict[chordName] = trebleChroma
+            # this is part of the validation set
+            # add observation to Xtest
+            Xtest.append(chroma)
 
-        # update state labels
-        QLabels.add(chordName)
-
-        obsNum += 1
+            # add the observations chord label
+            ytest.append(chordName)
     
     QLabels = sorted(QLabels)
     N = len(QLabels)
@@ -115,7 +130,7 @@ def learnHMM(M, addOne = True, features = 't', covType = 'full'):
         
         # fill B
         if q in bDict:
-            #print "learning emissions for chord: %s, index: %d, #obs: %d" % (q, i, bDict[q].shape[0])
+            print "learning emissions for chord: %s, index: %d, #obs: %d" % (q, i, bDict[q].shape[0])
             bGMM = GMM(M, 12, covType, zeroCorr=0.01, convEps=1e-6)
             bGMM.expectMax(bDict[q], maxIter=100)
             B.append(bGMM)
@@ -131,6 +146,8 @@ def learnHMM(M, addOne = True, features = 't', covType = 'full'):
     pi /= np.sum(pi)
     A /= A.sum(axis=1)[:,np.newaxis]
 
-    return pi, A, B, QLabels
+    Xtest = np.asarray(Xtest).squeeze()
 
-#pi, A, B, labels = learnHMM(3, covType='diag')
+    return pi, A, B, QLabels, Xtest, ytest
+
+#pi, A, B, labels, Xtest, ytest = learnHMM(3, covType='diag')
